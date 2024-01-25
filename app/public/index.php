1aaa8ff628c08d2381243db049fe107ddca6db0d
Ryan Prather <godsgood33@gmail.com>
2024-01-25 11:49:42 -0500

    Fix buttons Add fontawesome Add conf editor button Formatting

--- app/public/index.php
+++ app/public/index.php
@@ -5,46 +5,62 @@ $conf = new Config(file_get_contents(dirname(__DIR__).'/config/conf.json'));
 
 $user = ($_COOKIE['user'] ?? "");
 
-$tag = '/tag/'.htmlentities(strtolower(str_replace(' ', '-', $conf->getUserHomeTag($user))));
-if(isset($_GET['tag'])) {
+$hometag = htmlentities(strtolower(str_replace(' ', '-', $conf->getUserHomeTag($user))));
+$tag = '/tag/'.$hometag;
+if(isset($_GET['tag']) && $_GET['tag']) {
     $tag = '/tag/'.htmlentities(strtolower(str_replace(' ', '-', $_GET['tag'])));
 }
 
 ?>
 
 <html>
-    <head>
-        <title><?php print $conf->getTitle(); ?></title>
-        <link href='/css/style.css' rel='stylesheet' />
-    </head>
-    <body>
-        <div id='categories'>
-            <select id='user' onchange='javascript:setCookie("user", this.value);location.reload();'>
-                <option value=''>User...</option>
-                <?php
+
+<head>
+    <title><?php print $conf->getTitle(); ?></title>
+    <link href='/css/style.css' rel='stylesheet' />
+    <script src="https://kit.fontawesome.com/f15a79324f.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div id='categories'>
+        <select id='user' onchange='javascript:setCookie("user", this.value);location.reload();'>
+            <option value=''>User...</option>
+            <?php
                 foreach($conf->getUsers() as $u) {
                     $selected = null;
                     if ($user == ucfirst($u)) {
                         $selected = " selected";
                     }
-                    print "<option val='$u'{$selected}>".ucfirst($u)."</option>\n";
+                    print "<option val='$u'{$selected}>".ucfirst($u)."</option>".PHP_EOL;
                 }
-                ?>
-            </select>
-            <ul>
-                <?php
-                foreach($conf->getUserTags($user) as $t) {
-                    $taglink = htmlentities(strtolower(str_replace(' ', '-', $t)));
-                    print "<li><a href='/?tag=$taglink'>$t</a></li>\n";
-                } ?>
-            </ul>
+?>
+        </select>
+        <ul>
+            <?php
+foreach($conf->getUserTags($user) as $t) {
+    $selected = null;
+    $taglink = htmlentities(strtolower(str_replace(' ', '-', $t)));
+    if (isset($_GET['tag']) && $taglink == $_GET['tag']) {
+        $selected = 'active';
+    } elseif (!isset($_GET['tag']) && $taglink == $hometag) {
+        $selected = 'active';
+    }
+    print "<li><a class='myButton {$selected}' href='/?tag={$taglink}'>{$t}</a></li>\n";
+}
+?>
+        </ul>
+        <div style='float:right;'>
+            <a href='/config.php'><i class='fa-solid fa-gears'></i></a>
         </div>
-        <iframe id='heimdall' src="<?php print $conf->getHeimdallString().$tag; ?>"></iframe>
-        <script type='text/javascript'>
-            // Set a Cookie
-            function setCookie(cName, cValue) {
-                    document.cookie = cName + "=" + cValue + "; path=/";
-            }
-        </script>
-    </body>
+    </div>
+    <iframe id='heimdall'
+        src="<?php print $conf->getHeimdallString().$tag; ?>"></iframe>
+    <script type='text/javascript'>
+        // Set a Cookie
+        function setCookie(cName, cValue) {
+            document.cookie = cName + "=" + cValue + "; path=/";
+        }
+    </script>
+</body>
+
 </html>
